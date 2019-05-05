3a69b8d4281599e5c4fb4f84bb1e9e587f12539e
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2019-05-05 02:05:49 -0300

    - Redirect the /contact to ContactForm component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { FrameComponent } from './pages/master/frame/frame.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { ContactFormComponent } from './pages/contact-form/contact-form.component';
 
 const routes: Routes = [
   {
     path: '',
     component: FrameComponent,
     children: [
-      { path: '', component: WelcomeComponent }
+      { path: '', component: WelcomeComponent },
+      { path: 'contact', component: ContactFormComponent }
     ]
   }
 ];
