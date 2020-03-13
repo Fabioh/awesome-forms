7112a2831699fe50ff2aa580df3d745a61ebb026
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2020-03-13 17:44:16 -0300

    - Update @angular\material to version 8

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
